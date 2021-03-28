bec301b92bd88feb40d2e3f1f09bb948cc8fdfe6
Olya <olgarkovich@gmail.com>
2021-03-28 16:48:04 +0300

    add animation localStorage hotfix

--- public/detail.js
+++ public/detail.js
@@ -1,8 +1,15 @@
+const detailName = document.querySelector('#detail-name')
+const detailDescription = document.querySelector('#detail-description')
+const comments = document.querySelector('#comments')
+const form = document.querySelector('#input-form')
+
+let d
+
 function getCookie(cname) {
     const name = cname + "=";
     const decodedCookie = decodeURIComponent(document.cookie);
     const ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
+    for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) === ' ') {
             c = c.substring(1);
@@ -17,32 +24,55 @@ function getCookie(cname) {
 function renderTeaDetails(doc) {
     detailName.textContent = doc.data().name
     detailDescription.textContent = doc.data().description
-    detailImg.src = doc.data().picture
 
+    let img = document.createElement('img')
+    img.src = doc.data().picture
+    img.classList.add("left-img")
+    img.alt = "tea"
+    img.width = 170
+    img.height = 170
+    img.style.margin = 10
 
+    detailDescription.appendChild(img)
 
-    let commentList = doc.data().comments
-    
-    // for (let comment in commentList) {
-    //     let div = document.createElement('div')
-    //     div.classList.add('comment-form')
-    //     let p = document.createElement('p')
-    //     p.textContent = comment
-    //     console.log(comment)
-    //     console.log(comment.toString())
-    //     div.appendChild(p)
-    //     comments.appendChild(div)
-    // }
+    let commentArray = getCommentArray(doc)
+
+    console.log(commentArray.length)
+
+    commentArray.forEach(comment => {
+            let div = document.createElement('div')
+            div.classList.add('comment-form')
+            let p = document.createElement('p')
+            p.textContent = comment
+            console.log(comment)
+            div.appendChild(p)
+            comments.appendChild(div)
+        }
+    )
 }
 
-const detailName = document.querySelector('#detail-name')
-const detailDescription = document.querySelector('#detail-description')
-const detailImg = document.querySelector('#detail-img')
-const comments = document.querySelector('#comments')
+function getCommentArray(doc) {
+    let commentList = doc.data().comments
+    return commentList.split(',')
+}
+
 db.collection('tea-type').doc(getCookie('id')).collection('teaList').get(getCookie('name')).then((snapshot) => {
     snapshot.docs.forEach(doc => {
         if (getCookie('name') === doc.id) {
+            d = doc
+            console.log('d = doc')
             renderTeaDetails(doc)
         }
     })
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let comments = getCommentArray(d)
+    db.collection('tea-type').doc(getCookie('id')).collection('teaList').doc(getCookie('name')).update({
+        comments: comments + ', ' + form.comment.value
+    })
+    setTimeout(function () {
+        location.reload();
+    }, 1000);
 })
